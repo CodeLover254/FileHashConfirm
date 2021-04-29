7cf6ead34c7416365d63ac7173dff3ee0fc8d33b
CodeLover254 <astroguypasa@gmail.com>
2021-04-29 13:56:29 +0300

    Tweaked the method by which the fxml file is obtained.

---
+++ Java/FileHashConfirm/src/main/java/org/limepietechnologies/App.java
@@ -0,0 +1,38 @@
+package org.limepietechnologies;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ *
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("AppForm"));
+        stage.getIcons().add(new Image(App.class.getResourceAsStream("app_logo.png")));
+        stage.setTitle("FileHashConfirm");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
