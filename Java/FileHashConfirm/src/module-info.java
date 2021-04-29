9a6be51344f878a54946a1a1d7c9f7c94b261a38
CodeLover254 <astroguypasa@gmail.com>
2021-04-29 13:15:24 +0300

    removed old source in favour of maven

--- Java/FileHashConfirm/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/*
-module FileHashConfirm {
-    requires javafx.base;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.google.common;
-
-    opens com.filehashconfirm;
-}*/
