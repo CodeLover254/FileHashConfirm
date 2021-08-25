caedc614d5e1bb6d5dda4497008e318cdadc79ea
CodeLover254 <astroguypasa@gmail.com>
2021-08-25 10:25:56 +0300

    Removed google commons module

--- Java/FileHashConfirm/src/main/java/module-info.java
+++ Java/FileHashConfirm/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module org.limepietechnologies {
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.google.common;
 
     opens org.limepietechnologies to javafx.fxml;
     exports org.limepietechnologies;
